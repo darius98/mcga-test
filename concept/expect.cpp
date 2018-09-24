e3ee3a629fab7be5b7cf035faf7af81f5be36a71
Darius <marian.darius98@gmail.com>
2018-09-24 23:07:54 +0000

    * Added "expect" equivalents for some "expectMatches" in expect concept code

--- concept/expect.cpp
+++ concept/expect.cpp
@@ -10,36 +10,30 @@ using namespace std;
 
 int main() {
     vector<int> v{1, 2, 3};
+    test("", [&]() {
+        expect(1 + 2 == 3);
+        expectMatches(1 + 2 == 3, isTrue);
+        expect(1 + 2 != 4);
+        expectMatches(1 + 2 == 4, isFalse);
+        expectMatches(1 + 2 == 4, isNot(isTrue));
+        expectMatches(1 + 2 == 3, isNot(isFalse));
 
-    expect(1 + 2 == 3, isTrue);
-    expect(1 + 2 == 4, isFalse);
+        expect(1 + 2 == 3 && 1 + 2 >= 2);
+        expectMatches(1 + 2, both(isEqualTo(3), isGreaterThanOrEqualTo(2)));
 
-    expect(1 + 2 == 3, isNot(isFalse));
-    expect(1 + 2 == 4, isNot(isTrue));
-    expect(1 + 2, both(isEqualTo(3), isGreaterThanOrEqualTo(2)));
+        expectMatches(v, isNotEmpty);
+        expectMatches(v, hasSize(3));
+        expectMatches(v, hasSize(isGreaterThan(2)));
+        expectMatches(v, hasSize(isGreaterThanOrEqualTo(2)));
+        expectMatches(v, hasSize(isGreaterThanOrEqualTo(3)));
+        expectMatches(v, hasSize(isLessThan(4)));
+        expectMatches(v, hasSize(isLessThanOrEqualTo(4)));
+        expectMatches(v, hasSize(isLessThanOrEqualTo(3)));
 
-    expect(v, isNotEmpty);
-    expect(v, hasSize(3));
-    expect(v, hasSize(isGreaterThan(2)));
-    expect(v, hasSize(isGreaterThanOrEqualTo(2)));
-    expect(v, hasSize(isGreaterThanOrEqualTo(3)));
-    expect(v, hasSize(isLessThan(4)));
-    expect(v, hasSize(isLessThanOrEqualTo(4)));
-    expect(v, hasSize(isLessThanOrEqualTo(3)));
+        expectMatches(v, isEqualTo(vector<int>{1, 2, 3}));
+        expectMatches(v, isIdenticalTo(v));
 
-    expect(v, isEqualTo(vector<int>{1, 2, 3}));
-    expect(v, isIdenticalTo(v));
-
-    expect(v, either(isEmpty, isNotEmpty));
-
-    try {
-        expect(v, hasSize(isGreaterThan(5)));
-    } catch(const ExpectationFailed& failure) {
-        assert(
-            failure.getMessage() ==
-            "expected container of size greater than '5', got '3': '[1, 2, 3]'"
-        );
-    }
-
+        expectMatches(v, either(isEmpty, isNotEmpty));
+    });
     return 0;
 }
