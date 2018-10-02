2c3f1ce875b133700cf63d17581950d8671ff2f8
Darius <marian.darius98@gmail.com>
2018-10-02 10:52:21 +0300

    * More refactoring to CMakeLists + add new wiki examples

---
+++ wiki_examples/set_up_tear_down_bad.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <runtime_testing>
+
+int main() {
+    std::vector<int> v;
+
+    test("After pushing back 3, 4 and 5, v has size 3", [&]() {
+        v.push_back(3);
+        v.push_back(4);
+        v.push_back(5);
+        expect(v.size() == 3);
+        v.clear();
+    });
+
+    test("After pushing back 3, 4 and 5, v is equal to {3, 4, 5}", [&]() {
+        v.push_back(3);
+        v.push_back(4);
+        v.push_back(5);
+        expect(v == std::vector<int>{3, 4, 5});
+        v.clear();
+    });
+
+    test("After pushing back 3, 4 and 5, v does not contain '6'", [&]() {
+        v.push_back(3);
+        v.push_back(4);
+        v.push_back(5);
+        expect(std::find(v.begin(), v.end(), 6) == v.end());
+        v.clear();
+    });
+
+    return runtime_testing::finalizeTesting();
+}
---
+++ wiki_examples/set_up_tear_down_good.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <runtime_testing>
+
+int main() {
+    std::vector<int> v;
+
+    setUp([&]() {
+        v.push_back(3);
+        v.push_back(4);
+        v.push_back(5);
+    });
+
+    tearDown([&]() {
+        v.clear();
+    });
+
+    test("After pushing back 3, 4 and 5, v has size 3", [&]() {
+        expect(v.size() == 3);
+    });
+
+    test("After pushing back 3, 4 and 5, v is equal to {3, 4, 5}", [&]() {
+        expect(v == std::vector<int>{3, 4, 5});
+    });
+
+    test("After pushing back 3, 4 and 5, v does not contain '6'", [&]() {
+        expect(std::find(v.begin(), v.end(), 6) == v.end());
+    });
+
+    return runtime_testing::finalizeTesting();
+}
