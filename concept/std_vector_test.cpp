fe906ef9197e9f288fe366ba8dae1fafdbbed2f9
Darius <marian.darius98@gmail.com>
2018-10-05 04:42:25 +0000

    * Removed comments in vector testing concept

--- concept/std_vector_test.cpp
+++ concept/std_vector_test.cpp
@@ -22,33 +22,13 @@ int main(int argc, char** argv) {
     vector<int> v, w;
 
     setUp([&]() {
-        /**
-         * This lambda expression is called before every test.
-         *
-         * We want each test to start with a couple of clean vectors.
-         * To avoid writing bug-prone boiler-plate at the start of each test,
-         * write the initialization in a setUp instead.
-         */
         v = vector<int>();
         w = vector<int>();
     });
 
     test("Vector is initially empty - 0 (P)", [&]() {
-        /**
-         * This is the main way to assert conditions during tests.
-         * expect(BOOLEAN_EXPRESSION) will fail and stop the test if the
-         * expression does not evaluate to true.
-         */
         expect(v.empty());
-        /**
-         * This is the exact equivalent for verifying a boolean condition, using
-         * the `isTrue` matcher.
-         */
         expectMatches(v.empty(), isTrue);
-        /**
-         * This is equivalent to the above, but with a nicer error message in
-         * case of failure (see versions 1 and 2 of this test).
-         */
         expectMatches(v, isEmpty);
     });
 
@@ -56,13 +36,6 @@ int main(int argc, char** argv) {
         // When the vector is not empty, the isEmpty expectation will fail.
         v = {1, 2, 3};
 
-        /**
-         * This is the non-graceful failure - where no extra information is
-         * available except the boolean expression that was evaluated and
-         * failed.
-         * This should display something similar to
-         * `v.empty() is false`
-         */
         expect(v.empty()); // Fails!
     });
 
@@ -70,12 +43,6 @@ int main(int argc, char** argv) {
         // When the vector is not empty, the isEmpty expectation will fail.
         v = {1, 2, 3};
 
-        /**
-         * This is the graceful failure - where all information is available:
-         * what was expected, what object was provided and WHY it failed.
-         * This should display something similar to
-         * `Expected empty iterable. Got '[1, 2, 3]': non-empty iterable`
-         */
         expectMatches(v, isEmpty); // Fails!
     });
 
@@ -96,63 +63,13 @@ int main(int argc, char** argv) {
         expectMatches(v, isIdenticalTo(w));
     });
 
-    /**
-     * This is a group of tests. You can look at it as a mini-suite of
-     * tests, integrated in the overall suite defined by the file. A group
-     * can be used to further reduce boiler-plate from test names
-     * (the description of the group is pre-pended to the test description)
-     * and (as you will see soon)
-     */
     group("Inserting elements", [&]() {
         int* a;
 
-        /**
-         * This is an in-group setUp. It does NOT overwrite the global
-         * setUp, but it is rather an addition to it. This will be executed
-         * before each test in this group and its subgroups, after the
-         * global setUp.
-         *
-         * In abstract, if you have the configuration:
-         *
-         * ```cpp
-         * setUp(SetUpFunc0);
-         * tearDown(TearDownFunc0);
-         *
-         * group(... {
-         *   setUp(SetUpFunc1);
-         *   tearDown(TearDownFunc1);
-         *   group(...{
-         *     ...
-         *              test(TestFunc);
-         *     ...
-         *   }
-         * });
-         * ```
-         *
-         * then the order of functions called will be
-         *
-         * ```cpp
-         * SetUpFunc0();
-         * SetUpFunc1();
-         * ...
-         * SetUpFuncN();
-         * TestFunc(); // execute the actual test
-         * TearDownFuncN();
-         * ...
-         * TearDownFunc1();
-         * TearDownFunc0();
-         * ```
-         */
         setUp([&]() {
             a = new int[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         });
 
-        /**
-         * This is the tearDown function. It is called after each test in the
-         * group no matter whether it passed or failed. Generally, you can
-         * use this space to clean up declared memory / delete created files
-         * etc. Its behaviour is symmetrical to the setUp function.
-         */
         tearDown([&]() {
             delete[] a;
         });
@@ -215,12 +132,6 @@ int main(int argc, char** argv) {
             v = {10, 11, 12, 13, 14, 15, 16};
             v.insert(v.begin() + 5, 0);
 
-            /**
-             * Here are some examples of comparison matchers.
-             *
-             * These would fail at compile time if provided with un-matching
-             * types (i.e. `expectMatches(2, isEqualTo({1, 2}))`);
-             */
             expectMatches(v[5], isEqualTo(0));
             expectMatches(v[5], isLessThan(2));
             expectMatches(v[5], isLessThanOrEqualTo(2));
@@ -229,31 +140,16 @@ int main(int argc, char** argv) {
             expectMatches(v[5], isGreaterThanOrEqualTo(-2));
             expectMatches(v[5], isGreaterThanOrEqualTo(0));
 
-            /**
-             * Check THIS out! Matchers can be nested too!
-             */
             expectMatches(v, hasSize(isGreaterThan(7)));
-
-            /**
-             * You can also have matchers for each element!
-             */
             expectMatches(v, eachElement(isGreaterThanOrEqualTo(0)));
             expectMatches(v, anyElement(isEqualTo(13)));
 
-            /**
-             * You can compose matchers together through both, either and isNot.
-             */
             expectMatches(v, eachElement(either(isZero, isGreaterThan(9))));
             expectMatches(v, anyElement(both(isOdd, isGreaterThan(10))));
             expectMatches(v, anyElement(both(isEven, isGreaterThan(10))));
             expectMatches(v, eachElement(isNot(isNegative)));
         });
 
-        /**
-         * The following group of tests will each fail one of the matchers
-         * above, so you can see the auto-generated description of nested
-         * matchers.
-         */
         group("Failing versions of above test", [&]() {
             setUp([&]() { v = {10, 11, 12, 13, 14, 0, 15, 16}; });
 
@@ -333,9 +229,6 @@ int main(int argc, char** argv) {
 
     group("Throwing", [&]() {
         test("Calling at for an out of bounds position throws - 0 (P)", [&]() {
-            /**
-             * You can also expect throws!
-             */
             expectMatches([&]() { v.at(2); }, throws);
             expectMatches([&]() { v.at(2); }, throwsA<out_of_range>());
         });
@@ -352,12 +245,5 @@ int main(int argc, char** argv) {
         });
     });
 
-    /**
-     * This is the only line of boiler-plate. This one is for integration with
-     * automated tools: it automatically generates a JSON file describing all
-     * tests executed, their structure and reasons for failure
-     * and returns a status-code equal to the number of tests failed. Thus,
-     * a passed suite of tests will return the exit code zero (0).
-     */
     return finalizeTesting();
 }
