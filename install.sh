20dae86a234fa8b69496a12dac6f433e1ddbc9aa
Darius <marian.darius98@gmail.com>
2018-10-08 03:29:02 +0000

    * Added some extra sudo-s to install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ git submodule update --init --recursive
 cd third_party/sandman
 make build
 sudo mv box /usr/local/bin/box
-mkdir -p /eval
-touch /eval/isolate.log
+sudo mkdir -p /eval
+sudo touch /eval/isolate.log
 sudo box --init
