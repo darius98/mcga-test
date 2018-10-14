5c314c87bb14410603b2b01f9833c9b0bd716098
Darius <marian.darius98@gmail.com>
2018-10-14 23:28:26 +0000

    * Install the library properly (in /usr/local/include) before using it

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 git submodule update --init --recursive
+
+# Copy files to include folder
+sudo cp -r kktest_lib/ /usr/local/include/kktest_src
+sudo cp -r third_party/ /usr/local/include/kktest_src/third_party
+sudo cp kktest_lib/src/kktest /usr/local/include/kktest
+sudo cp kktest_lib/src/kktest_matchers /usr/local/include/kktest_matchers
+sudo cp kktest_lib/kktest_library.cmake /usr/local/include/kktest.cmake
+
+# Install and init box
 cd third_party/sandman
 sudo apt install -y acl
 make build
--- kktest_lib/kktest_library.cmake
+++ kktest_lib/kktest_library.cmake
@@ -7,32 +7,36 @@ cmake_minimum_required(VERSION 3.7)
 project(KKTestLibs)
 set(CMAKE_CXX_STANDARD 14)
 
-add_definitions(-Dautojsonuselib)
-include_directories(${CMAKE_CURRENT_LIST_DIR}/../third_party/autojson/include)
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../third_party/autojson CMakeFiles_autojson)
+set(KKTEST_DIR "${CMAKE_CURRENT_LIST_DIR}/kktest_src")
+set(KKTEST_SRC_DIR "${KKTEST_DIR}/src")
+set(KKTEST_THIRD_PARTY_DIR "${KKTEST_DIR}/third_party")
 
-add_definitions(-Deasyflagsuselib)
-include_directories(${CMAKE_CURRENT_LIST_DIR}/../third_party/easyflags/include)
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../third_party/easyflags CMakeFiles_easyflags)
+set(AUTOJSON_DIR "${KKTEST_THIRD_PARTY_DIR}/autojson")
+set(AUTOJSON_INCLUDE_DIR "${AUTOJSON_DIR}/include")
+file(GLOB AUTOJSON_SRC ${AUTOJSON_DIR}/src/*.cpp)
+add_library(autojson ${AUTOJSON_SRC})
 
-include_directories(${CMAKE_CURRENT_LIST_DIR}/src)
-file(GLOB KKTestLibImpl
-        ${CMAKE_CURRENT_LIST_DIR}/src/testing/*.cpp
-        ${CMAKE_CURRENT_LIST_DIR}/src/testing/*.hpp
-        ${CMAKE_CURRENT_LIST_DIR}/src/utils/filename.hpp
-        )
-add_library("kktest" ${KKTestLibImpl} src/kktest)
+set(EASYFLAGS_DIR "${KKTEST_THIRD_PARTY_DIR}/easyflags")
+set(EASYFLAGS_INCLUDE_DIR "${EASYFLAGS_DIR}/include")
+add_library(easyflags "${EASYFLAGS_DIR}/src/EasyFlags.cpp")
+target_compile_definitions(easyflags PUBLIC easyflagsuselib)
+target_link_libraries(easyflags autojson)
+
+include_directories(${EASYFLAGS_INCLUDE_DIR} ${AUTOJSON_INCLUDE_DIR})
+add_definitions(-Deasyflagsuselib -Dautojsonuselib)
+
+file(GLOB KKTestLibImpl ${KKTEST_SRC_DIR}/testing/*.cpp)
+add_library(kktest ${KKTestLibImpl})
+target_include_directories(kktest PUBLIC ${KKTEST_SRC_DIR})
+target_include_directories(kktest PUBLIC ${AUTOJSON_INCLUDE_DIR})
+target_include_directories(kktest PUBLIC ${EASYFLAGS_INCLUDE_DIR})
 target_link_libraries(kktest autojson easyflags)
 
-file(GLOB KKTestMatchersLibImpl
-        ${CMAKE_CURRENT_LIST_DIR}/src/matcher/*.cpp
-        ${CMAKE_CURRENT_LIST_DIR}/src/matcher/*.hpp
-        ${CMAKE_CURRENT_LIST_DIR}/src/utils/filename.hpp
-        ${CMAKE_CURRENT_LIST_DIR}/src/utils/streamer.hpp
-        )
-add_library("kktest_matchers" ${KKTestMatchersLibImpl} src/kktest_matchers)
-
-include_directories(${CMAKE_CURRENT_LIST_DIR}/src)
+file(GLOB KKTestMatchersLibImpl ${KKTEST_SRC_DIR}/matcher/*.cpp)
+add_library(kktest_matchers ${KKTestMatchersLibImpl})
+target_include_directories(kktest_matchers PUBLIC ${KKTEST_SRC_DIR})
+target_include_directories(kktest_matchers PUBLIC ${AUTOJSON_INCLUDE_DIR})
+target_include_directories(kktest_matchers PUBLIC ${EASYFLAGS_INCLUDE_DIR})
 
 function(testcase TEST_NAME FILE_NAME)
     if (NOT TESTS_PATH)
