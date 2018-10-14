c23ea9d7a7d6a006d4d60ebcd1097ae5505f67f5
Darius <marian.darius98@gmail.com>
2018-10-14 13:24:02 +0000

    * Moved all stuff to a kktest_lib directory, to prepare for another project

--- kktest_lib/kktest_library.cmake
+++ kktest_lib/kktest_library.cmake
@@ -8,12 +8,12 @@ project(KKTestLibs)
 set(CMAKE_CXX_STANDARD 14)
 
 add_definitions(-Dautojsonuselib)
-include_directories(${CMAKE_CURRENT_LIST_DIR}/third_party/autojson/include)
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/third_party/autojson CMakeFiles_autojson)
+include_directories(${CMAKE_CURRENT_LIST_DIR}/../third_party/autojson/include)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../third_party/autojson CMakeFiles_autojson)
 
 add_definitions(-Deasyflagsuselib)
-include_directories(${CMAKE_CURRENT_LIST_DIR}/third_party/easyflags/include)
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/third_party/easyflags CMakeFiles_easyflags)
+include_directories(${CMAKE_CURRENT_LIST_DIR}/../third_party/easyflags/include)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../third_party/easyflags CMakeFiles_easyflags)
 
 include_directories(${CMAKE_CURRENT_LIST_DIR}/src)
 file(GLOB KKTestLibImpl
