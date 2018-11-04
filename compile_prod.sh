f4a9257a6dffb12b4d09a88960ac4b31489837da
Darius <marian.darius98@gmail.com>
2018-11-04 15:48:12 +0000

    * Simplified and renamed cmake function

--- compile_prod.sh
+++ compile_prod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cmake -Hkktest_lib -Bbuild/cmake_prod -DBUILD_TYPE:STRING=PROD
+cmake -Hkktest_lib -Bbuild/cmake_prod -DCMAKE_BUILD_TYPE=Release -DBUILD_TYPE:STRING=PROD
 cd build/cmake_prod && make
