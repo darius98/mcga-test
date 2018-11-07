b7a7f353cab314813303557945933ac01245d8e0
Darius <marian.darius98@gmail.com>
2018-11-07 10:31:13 +0000

    * Don't install Sandman/Box anymore

--- copy.sh
+++ copy.sh
@@ -14,17 +14,5 @@ sudo cp -r lib/include/* /usr/local/include
 # Copy the cmake library
 sudo cp lib/kktest.cmake /usr/local/include/kktest.cmake
 
-# Install and init box
-if ! [ -x "$(command -v box)" ]; then
-    sudo apt install -y acl
-    git clone https://github.com/establishment/sandman sandman
-    cd sandman && make build && cd ..
-    sudo mv sandman/box /usr/local/bin/box
-    rm -rf sandman
-    sudo mkdir -p /eval
-    sudo touch /eval/isolate.log
-fi
-sudo box --init
-
 # Copy the runner binary to a binary path
 sudo cp build/bin/runner /usr/local/bin/kktest
