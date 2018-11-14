287e927380d41e8c26bcd3cb4af486f3a8f33f65
Darius <marian.darius98@gmail.com>
2018-11-14 11:35:09 +0000

    * Moved all public headers in another subfolder layer in shared libraries   - Try to avoid name clashing issues

---
+++ common/strutil/include/strutil_public/unescape.hpp
@@ -0,0 +1,12 @@
+#ifndef COMMON_STRUTIL_INCLUDE_STRUTIL_PUBLIC_UNESCAPE_H_
+#define COMMON_STRUTIL_INCLUDE_STRUTIL_PUBLIC_UNESCAPE_H_
+
+#include <string>
+
+namespace strutil {
+
+std::string unescapeCharacters(const std::string& str);
+
+}
+
+#endif
