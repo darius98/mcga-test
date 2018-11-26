4838ee820b052c2748db058fc4541bf87eddcb20
Darius <marian.darius98@gmail.com>
2018-11-26 22:32:49 +0000

    * Cleaned up CMake files for the new extensions pattern

---
+++ build_kktest_shared_lib.cmake
@@ -0,0 +1,14 @@
+# TODO(darius98): This should be auto-generated from a list of extensions
+
+add_subdirectory(kktest_extensions/core_matchers)
+add_subdirectory(kktest_extensions/feedback)
+
+add_library(kktest SHARED $<TARGET_OBJECTS:kktest_impl>
+    $<TARGET_OBJECTS:kktest_extension_core_matchers>
+    $<TARGET_OBJECTS:kktest_extension_feedback>
+)
+
+target_link_libraries(kktest kktest_interface
+    kktest_extension_core_matchers
+    kktest_extension_feedback
+)
