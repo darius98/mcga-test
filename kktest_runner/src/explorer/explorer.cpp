1b5fd1b0b46a2bac3a9f2c3a49601493107d3a58
Darius <marian.darius98@gmail.com>
2018-11-29 10:15:24 +0200

    * Simplified explorer implementation

--- kktest_runner/src/explorer/explorer.cpp
+++ kktest_runner/src/explorer/explorer.cpp
@@ -6,6 +6,8 @@ using namespace fsystem;
 using namespace strutil;
 using namespace std;
 
+namespace {
+
 constexpr const size_t kkTestSigSize = 32;
 constexpr const size_t kkTestSigHalfSize = kkTestSigSize >> 1;
 const unsigned char kkTestSignatureFirstHalf[] =
@@ -30,65 +32,59 @@ bool containsKKTestSignature(unsigned char* buffer, size_t bufferSize) {
     return false;
 }
 
-class Explorer {
-public:
-    explicit Explorer(Folder _rootFolder): rootFolder(move(_rootFolder)) {}
-
-    void findTestCases(const function<void(File)>& onTestFound) const {
-        pair<vector<File>, vector<Folder>> children = rootFolder.children();
-        for (const File& file: children.first) {
-            if (isTestCase(file)) {
-                onTestFound(file);
-            }
-        }
-        for (const Folder& folder: children.second) {
-            Explorer(folder).findTestCases(onTestFound);
-        }
+bool isTestCase(const File& file) {
+    if (!file.isExecutable() || !file.isReadable()) {
+        return false;
     }
-
-private:
-    static bool isTestCase(const File& file) {
-        if (!file.isExecutable() || !file.isReadable()) {
-            return false;
-        }
-        string filePath = file.toString();
-        const char* filePathCStr = filePath.c_str();
-        // read the executable file, looking for the kktest signature.
-        // TODO: Move this somewhere else and make it more generic.
-        constexpr const size_t capacity = 1024;
-        unsigned char buffer[capacity];
-        FILE* filePtr = fopen(filePathCStr, "rb");
-        size_t readLen = fread(buffer, 1, capacity, filePtr);
-        if (ferror(filePtr) != 0) {
+    string filePath = file.toString();
+    const char* filePathCStr = filePath.c_str();
+    // read the executable file, looking for the kktest signature.
+    // TODO: Move this somewhere else and make it more generic.
+    constexpr const size_t capacity = 1024;
+    unsigned char buffer[capacity];
+    FILE* filePtr = fopen(filePathCStr, "rb");
+    size_t readLen = fread(buffer, 1, capacity, filePtr);
+    if (ferror(filePtr) != 0) {
+        // TODO: Handle.
+    }
+    if (readLen < kkTestSigSize) {
+        return false;
+    }
+    if (containsKKTestSignature(buffer, readLen)) {
+        return true;
+    }
+    while (!feof(filePtr)) {
+        memcpy(buffer, buffer + readLen - kkTestSigSize, kkTestSigSize);
+        readLen = fread(buffer + kkTestSigSize, 1, capacity - kkTestSigSize, filePtr);
+        readLen += 32;
+        if (ferror(filePtr)) {
             // TODO: Handle.
-        }
-        if (readLen < kkTestSigSize) {
-            return false;
         }
         if (containsKKTestSignature(buffer, readLen)) {
             return true;
         }
-        while (!feof(filePtr)) {
-            memcpy(buffer, buffer + readLen - kkTestSigSize, kkTestSigSize);
-            readLen = fread(buffer + kkTestSigSize, 1, capacity - kkTestSigSize, filePtr);
-            readLen += 32;
-            if (ferror(filePtr)) {
-                // TODO: Handle.
-            }
-            if (containsKKTestSignature(buffer, readLen)) {
-                return true;
-            }
-        }
-        return false;
     }
+    return false;
+}
 
-    Folder rootFolder;
-};
+void findTestCases(const Folder& folder, const function<void(File)>& onTestFound) {
+    pair<vector<File>, vector<Folder>> children = folder.children();
+    for (const File& file: children.first) {
+        if (isTestCase(file)) {
+            onTestFound(file);
+        }
+    }
+    for (const Folder& childFolder: children.second) {
+        findTestCases(childFolder, onTestFound);
+    }
+}
+
+}
 
 namespace runner {
 
 void explore(const function<void(File)>& onTestFound) {
-    Explorer(Folder(argumentRootFolder)).findTestCases(onTestFound);
+    findTestCases(Folder(argumentRootFolder), onTestFound);
 }
 
 }
