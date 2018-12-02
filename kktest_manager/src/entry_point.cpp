c7318f555aedc1427e2796d1d9f21e42e8a1ddec
Darius <marian.darius98@gmail.com>
2018-12-02 14:06:34 +0000

    * Kick-started the project of an extensions & general manager for the kktest ecosystem

---
+++ kktest_manager/src/entry_point.cpp
@@ -0,0 +1,26 @@
+#define VERSION "0.0.1"
+
+#include <iostream>
+
+#include <kktest_impl/arguments_api.hpp>
+
+using kktest::ArgumentsApi;
+using kktest::Flag;
+using kktest::String;
+using std::cout;
+using std::vector;
+
+int main(int argc, char** argv) {
+    ArgumentsApi* argumentsApi = ArgumentsApi::create("KKTest Manager.");
+
+    auto versionFlag = argumentsApi->addFlag("version", "Display program version.", "v");
+
+    vector<String> positional = argumentsApi->interpret(argc, argv);
+
+    if (versionFlag->get()) {
+        cout << "KKTest Manager version " << VERSION << "\n";
+    }
+
+    delete argumentsApi;
+    return 0;
+}
