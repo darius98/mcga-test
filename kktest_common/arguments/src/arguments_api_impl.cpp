f2189ec54d65892c87a96dbcce86a68be4ce34db
Darius <marian.darius98@gmail.com>
2018-12-09 12:25:42 +0000

    * Cleaned up and explained (through comments) the logic in the interpret method of the arguments library

--- kktest_common/arguments/src/arguments_api_impl.cpp
+++ kktest_common/arguments/src/arguments_api_impl.cpp
@@ -64,61 +64,83 @@ vector<String> ArgumentsApiImpl::interpret(int argc, char** argv) {
     for (CommandLineSpec* spec : commandLineSpecs) {
         spec->setDefault();
     }
+
     vector<String> positionalArguments;
     String lastShortName;
     bool onlyPositional = false;
     for (int i = 1; i < argc; ++i) {
         String arg(argv[i]);
+
+        // on encountering the "--" argument, all arguments from that point on
+        // are considered positional.
         if (arg == "--") {
             onlyPositional = true;
             continue;
         }
+
+        // all arguments after "--" are considered positional.
         if (onlyPositional) {
             positionalArguments.push_back(arg);
             continue;
         }
+
+        // an argument that does not start with '-' (or is equal to "-") is not
+        // considered special, and therefore will be treated like either a
+        // positional argument or a value filler for the last unfulfilled short
+        // name argument given in the format "-XYZ".
         if (!startsWith(arg, '-') || arg == "-") {
             if (lastShortName.empty()) {
+                // no unfulfilled argument given by short name, considering a
+                // positional argument.
                 positionalArguments.push_back(arg);
             } else {
+                // `lastShortName` is an unfulfilled argument given by short
+                // name in the format "-XYZ v" as "Z".
                 applyValue(lastShortName, arg);
                 lastShortName = "";
             }
             continue;
         }
 
+        // if we reached this point without hitting a `continue`, now the
+        // current argument is definitely a special one.
+
+        // if we had an unfulfilled argument given by short name, we will give
+        // it its implicit value since it won't be fulfilled by this argument.
         if (!lastShortName.empty()) {
             applyImplicit(lastShortName);
             lastShortName = "";
         }
 
-        // is of the form "-XYZ" or "--X" or "-XYZ=v" or "--X=v"
-        if (startsWith(arg, "--")) {
-            auto equalPos = arg.find('=');
-            if (equalPos == String::npos) {
-                // is of the form --X
-                applyImplicit(arg.substr(2));
-            } else {
-                // is of the form --X=v
-                applyValue(arg.substr(2, equalPos - 2),
-                           arg.substr(equalPos + 1));
+        auto equalPos = arg.find('=');
+
+        // 1. for "--X", give argument "X" its implicit value
+        if (startsWith(arg, "--") && equalPos == String::npos) {
+            applyImplicit(arg.substr(2));
+        }
+
+        // 2. for "--X=v", give argument "X" value "v"
+        if (startsWith(arg, "--") && equalPos != String::npos) {
+            applyValue(arg.substr(2, equalPos - 2), arg.substr(equalPos + 1));
+        }
+
+        // 3. for "-XYZ", give arguments "X" and "Y" their implicit values, and
+        // remember "Z" as the last short name, as a construct of the form
+        // "-XYZ v" is allowed, equivalent with "--X --Y --Z=v".
+        if (!startsWith(arg, "--") && equalPos == String::npos) {
+            for (size_t j = 1; j + 1 < arg.length(); ++j) {
+                applyImplicit(arg.substr(j, 1));
             }
-        } else {
-            auto equalPos = arg.find('=');
-            if (equalPos == String::npos) {
-                // is of the form -XYZ
-                for (size_t j = 1; j + 1 < arg.length(); ++j) {
-                    applyImplicit(arg.substr(j, 1));
-                }
-                lastShortName = arg.substr(arg.length() - 1, 1);
-            } else {
-                // is of the form -XYZ=v
-                for (size_t j = 1; j + 1 < equalPos; ++j) {
-                    applyImplicit(arg.substr(j, 1));
-                }
-                applyValue(arg.substr(equalPos - 1, 1),
-                           arg.substr(equalPos + 1));
+            lastShortName = arg.substr(arg.length() - 1, 1);
+        }
+
+        // 4. for "-XYZ=v", give arguments "X" and "Y" their implicit values and
+        // argument "Z" value "v".
+        if (!startsWith(arg, "--") && equalPos != String::npos) {
+            for (size_t j = 1; j + 1 < equalPos; ++j) {
+                applyImplicit(arg.substr(j, 1));
             }
+            applyValue(arg.substr(equalPos - 1, 1), arg.substr(equalPos + 1));
         }
     }
     if (!lastShortName.empty()) {
