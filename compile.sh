0c59d13fe392a2dfead6c786581fd99364d951c3
Darius Marian <marian.darius98@gmail.com>
2018-12-12 20:52:29 +0200

    * Changed the paths of generated binaries & libraries

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cmake -H. -Bbuild/cmake_dev -DCMAKE_BUILD_TYPE=Debug -DBUILD_TYPE:STRING=DEV
-cd build/cmake_dev && make -j4 && cd ../..
+cmake -H. -Bcmake_dev -DCMAKE_BUILD_TYPE=Debug -DBUILD_TYPE:STRING=DEV
+cd cmake_dev && make -j4 && cd ..
