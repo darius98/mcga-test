82fca417fc9c6ea336d0b6bf5c42cebb8260c928
Darius Marian <marian.darius98@gmail.com>
2018-12-13 12:52:53 +0200

    * Some cleanup in arguments library's tests

--- kktest_common/arguments/tests/arguments_test.cpp
+++ kktest_common/arguments/tests/arguments_test.cpp
@@ -23,100 +23,94 @@ using std::size_t;
 using std::initializer_list;
 using std::vector;
 
-vector<String> interpretArgs(ArgumentsApi* api,
-                             const initializer_list<String>& args) {
-    size_t cStyleArgsSize = (args.size() + 2) * sizeof(char*);
-    char** cStyleArgs = static_cast<char**>(malloc(cStyleArgsSize));
-    cStyleArgs[0] = copyAsCString("binaryName");
-    int index = 1;
-    for (const String& arg: args) {
-        cStyleArgs[index++] = copyAsCString(arg);
-    }
-    int numCArgs = static_cast<int>(args.size() + 1);
-    cStyleArgs[numCArgs] = nullptr;
-    auto positional = api->interpret(numCArgs, cStyleArgs);
-    for (size_t i = 0; i < numCArgs; ++i) {
-        free(cStyleArgs[i]);
-    }
-    free(cStyleArgs);
-    return positional;
-}
-
 void kkTestCase(Arguments) {
     ArgumentsApi* api = nullptr;
 
-    setUp([&api] { api = ArgumentsApi::create("Help prefix."); });
-    tearDown([&api] { delete api; api = nullptr; });
+    auto interpretArgs = [&](const initializer_list<String>& args) {
+        size_t cStyleArgsSize = (args.size() + 2) * sizeof(char*);
+        char** cStyleArgs = static_cast<char**>(malloc(cStyleArgsSize));
+        cStyleArgs[0] = copyAsCString("binaryName");
+        int index = 1;
+        for (const String& arg: args) {
+            cStyleArgs[index++] = copyAsCString(arg);
+        }
+        int numCArgs = static_cast<int>(args.size() + 1);
+        cStyleArgs[numCArgs] = nullptr;
+        auto positional = api->interpret(numCArgs, cStyleArgs);
+        for (size_t i = 0; i < numCArgs; ++i) {
+            free(cStyleArgs[i]);
+        }
+        free(cStyleArgs);
+        return positional;
+    };
 
-    group("Single argument", [&api] {
-        group("String argument", [&api] {
-            Argument* arg = nullptr;
+    setUp([&] { api = ArgumentsApi::create("Help prefix."); });
+    tearDown([&] { delete api; api = nullptr; });
 
-            setUp([&api, &arg] {
-                arg = api->addArgument("name", "Description.", "n", "a", "b");
-            });
+    group("Single string argument", [&] {
+        Argument* arg = nullptr;
 
-            test("no value provided leads to argument taking default value",
-                 [&api, &arg] {
-                interpretArgs(api, {});
-                expect(arg->get(), isEqualTo("a"));
-            });
+        setUp([&] {
+            arg = api->addArgument("name", "Description.", "n", "a", "b");
+        });
 
-            test("provided with single dash, short name", [&api, &arg] {
-                interpretArgs(api, {"-n"});
-                expect(arg->get(), isEqualTo("b"));
-            });
+        test("no value provided leads to argument taking default value", [&] {
+            interpretArgs({});
+            expect(arg->get(), isEqualTo("a"));
+        });
 
-            test("provided with double dash, short name", [&api, &arg] {
-                interpretArgs(api, {"--n"});
-                expect(arg->get(), isEqualTo("b"));
-            });
+        test("provided with single dash, short name", [&] {
+            interpretArgs({"-n"});
+            expect(arg->get(), isEqualTo("b"));
+        });
 
-            test("provided with double dash, long name", [&api, &arg] {
-                interpretArgs(api, {"--name"});
-                expect(arg->get(), isEqualTo("b"));
-            });
+        test("provided with double dash, short name", [&] {
+            interpretArgs({"--n"});
+            expect(arg->get(), isEqualTo("b"));
+        });
 
-            test("value provided with single dash & space", [&api, &arg] {
-                interpretArgs(api, {"-n", "v"});
-                expect(arg->get(), isEqualTo("v"));
-            });
+        test("provided with double dash, long name", [&] {
+            interpretArgs({"--name"});
+            expect(arg->get(), isEqualTo("b"));
+        });
 
-            test("value provided with single dash & equal sign", [&api, &arg] {
-                interpretArgs(api, {"-n=v"});
-                expect(arg->get(), isEqualTo("v"));
-            });
+        test("value provided with single dash & space", [&] {
+            interpretArgs({"-n", "v"});
+            expect(arg->get(), isEqualTo("v"));
+        });
 
-            test("value provided with double dash & space is positional, while "
-                 "the argument takes implicit value", [&api, &arg] {
-                auto positionalArgs = interpretArgs(api, {"--name", "v"});
-                expect(arg->get(), isEqualTo("b"));
-                expect(positionalArgs, isEqualTo(vector<String>{"v"}));
-            });
+        test("value provided with single dash & equal sign", [&] {
+            interpretArgs({"-n=v"});
+            expect(arg->get(), isEqualTo("v"));
+        });
 
-            test("value provided with double dash & equal sign (short name)",
-                 [&api, &arg] {
-                interpretArgs(api, {"--n=v"});
-                expect(arg->get(), isEqualTo("v"));
-            });
+        test("value provided with double dash & space is positional, while "
+             "the argument takes implicit value", [&] {
+            auto positionalArgs = interpretArgs({"--name", "v"});
+            expect(arg->get(), isEqualTo("b"));
+            expect(positionalArgs, isEqualTo(vector<String>{"v"}));
+        });
 
-            test("value provided with double dash & equal sign (long name)",
-                 [&api, &arg] {
-                interpretArgs(api, {"--name=v"});
-                expect(arg->get(), isEqualTo("v"));
-            });
+        test("value provided with double dash & equal sign (short name)", [&] {
+            interpretArgs({"--n=v"});
+            expect(arg->get(), isEqualTo("v"));
+        });
 
-            test("providing value for different argument name does not influence"
-                 " interesting argument", [&api, &arg] {
-                interpretArgs(api, {"-m", "v"});
-                expect(arg->get(), isEqualTo("a"));
-            });
+        test("value provided with double dash & equal sign (long name)", [&] {
+            interpretArgs({"--name=v"});
+            expect(arg->get(), isEqualTo("v"));
+        });
 
-            test("providing consecutive values to the same argument "
-                 "considers only the last one", [&api, &arg] {
-                interpretArgs(api, {"-n", "v1", "-n", "--name=v2"});
-                expect(arg->get(), isEqualTo("v2"));
-            });
+        test("providing value for different argument name does not influence"
+             " interesting argument", [&] {
+            interpretArgs({"-m", "v"});
+            expect(arg->get(), isEqualTo("a"));
+        });
+
+        test("when providing multiple values for one argument, it takes the "
+             "last one", [&] {
+            interpretArgs({"-n", "v1", "-n", "--name=v2"});
+            expect(arg->get(), isEqualTo("v2"));
         });
     });
 }
