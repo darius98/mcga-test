539869ba519e622ff55cf5a2822336a6de0c718d
Darius Marian <marian.darius98@gmail.com>
2019-01-17 22:04:43 +0000

    * Add install rules

--- generated/build_kktest.cmake
+++ generated/build_kktest.cmake
@@ -11,9 +11,16 @@ add_library(kktest $<TARGET_OBJECTS:kktest_impl>
 
 )
 
+install(TARGETS kktest ARCHIVE DESTINATION lib)
+
 target_link_libraries(kktest kktest_interface
 
     kktest_extension_core_matchers
     kktest_extension_feedback
 
 )
+
+install(TARGETS kktest_test_runner RUNTIME DESTINATION kktest_bin)
+
+install(DIRECTORY extensions/feedback/include DESTINATION .)
+install(DIRECTORY extensions/core_matchers/include DESTINATION .)
