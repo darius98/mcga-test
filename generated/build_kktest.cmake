2145368455b44691f6394ce2fdd20b49c2260940
Darius Marian <marian.darius98@gmail.com>
2019-01-17 22:34:03 +0000

    * Fixed CMake files so the final kktest library generated can be successfully installed

--- generated/build_kktest.cmake
+++ generated/build_kktest.cmake
@@ -3,11 +3,18 @@ add_subdirectory(extensions/core_matchers)
 add_subdirectory(extensions/feedback)
 add_subdirectory(extensions/test_runner)
 
-add_library(kktest $<TARGET_OBJECTS:kktest_impl>
+add_library(kktest
     generated/kktest_main.cpp
+
+    $<TARGET_OBJECTS:arguments>
+    $<TARGET_OBJECTS:interproc>
+    $<TARGET_OBJECTS:kktest_string>
+    $<TARGET_OBJECTS:kktest_time>
+    $<TARGET_OBJECTS:kktest_impl>
 
     $<TARGET_OBJECTS:kktest_extension_core_matchers>
     $<TARGET_OBJECTS:kktest_extension_feedback>
+    $<TARGET_OBJECTS:kktest_extension_feedback_test_logger>
 
 )
 
