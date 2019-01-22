2ade298229af51e24ef98f7bd7c80a7a1edd1cdf
Darius Marian <marian.darius98@gmail.com>
2019-01-22 13:40:53 +0000

    * Update install script manager paths

--- install.sh
+++ install.sh
@@ -2,11 +2,10 @@
 
 mkdir -p generated
 
-cp kktest_manager/templates/build_kktest.cmake.template generated/build_kktest.cmake
-cp kktest_manager/templates/kktest_main.cpp.template generated/kktest_main.cpp
+cp manager/templates/build_kktest.cmake.template generated/build_kktest.cmake
+cp manager/templates/kktest_main.cpp.template generated/kktest_main.cpp
 
 cmake -H. -Bcmake_build -DCMAKE_BUILD_TYPE=Release
 cd cmake_build
-make
-sudo make install
+make && sudo make install
 cd ..
