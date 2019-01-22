19ff039d8a52aa94f4efe8146eb6b73dd3406e3b
Darius Marian <marian.darius98@gmail.com>
2019-01-22 13:05:41 +0000

    Update install script to also run cat install.sh >install chmod a+x install

--- install.sh
+++ install.sh
@@ -6,4 +6,7 @@ cp kktest_manager/templates/build_kktest.cmake.template generated/build_kktest.c
 cp kktest_manager/templates/kktest_main.cpp.template generated/kktest_main.cpp
 
 cmake -H. -Bcmake_build -DCMAKE_BUILD_TYPE=Release
-cd cmake_build && make && cd ..
+cd cmake_build
+make
+sudo make install
+cd ..
