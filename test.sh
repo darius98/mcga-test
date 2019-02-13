8fc50dbffc988de6cd459a6277a97dbd78a629dc
Darius Marian <marian.darius98@gmail.com>
2019-02-13 12:17:18 +0200

    * Move tests to a test script

---
+++ test.sh
@@ -0,0 +1,29 @@
+# TODO(darius98): Re-write this in Python, and actually parse cmake's output to find the test cases.
+
+mkdir -p build || { echo 'making build directory failed' ; exit 1; }
+cd build || { echo 'moving to build directory failed' ; exit 1; }
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} || { echo 'CMake generation failed' ; exit 1; }
+
+function fail_build {
+    echo building $1 failed.;
+    exit 1;
+}
+
+function fail_run {
+    echo $1 failed.;
+    exit 1;
+}
+
+function test {
+    for var in "$@"
+    do
+        make $var"_test" -j2 || fail_build $var;
+        ./tests/$var"_test" || fail_run $var;
+    done
+}
+
+test common_interproc \
+     common_utils \
+     common_string \
+     kktest_extension_core_matchers \
+     kktest_extension_death
