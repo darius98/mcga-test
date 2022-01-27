c03fdee7bcc4ac3a9432f886f3e6b7ee4df6cd48
Darius Marian <marian.darius98@gmail.com>
2022-01-27 17:31:30 +0200

    Add a build mode for freestanding (still working on it).

---
+++ tests/integration/freestanding.cpp
@@ -0,0 +1,29 @@
+#include <mcga/test.hpp>
+
+#include "core/main.hpp"
+#include "extensions/feedback/exit_code_extension.hpp"
+
+using namespace mcga::test;
+
+const auto t = TestCase{"test case"} + []() {
+    test("passing-test", [] {
+        expect(1 + 1 == 2);
+    });
+
+    test("failing-test", [] {
+        expect(1 + 1 == 3);
+    });
+};
+
+int main() {
+    ExitCodeExtension exitCodeExtension(false, false);
+    auto extensions = makeExtensionArray(&exitCodeExtension);
+    const auto options = EntryPointOptions{
+      .extensions = extensions.data(),
+      .numExtensions = extensions.size(),
+      .numRuns = 1,
+    };
+    SmoothExecutor executor;
+    runTests(&executor, options);
+    return exitCodeExtension.getExitCode();
+}
